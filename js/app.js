f12d167bcf09a82a039d6c2ba48689dc48d6f5ba
Alexander Allen <aallen@blazinweb.com>
2015-01-24 20:28:06 -0600

    got our "skateboard" working

--- js/app.js
+++ js/app.js
@@ -10,11 +10,51 @@ function app(){
         {url: "./dist/style.css"},
         //js
         {url: "./bower_components/jquery/dist/jquery.min.js"},
-        {url: "./bower_components/lodash/dist/lodash.min.js"}
+        {url: "./bower_components/lodash/lodash.min.js"}
         // {url: "./bower_components/backbone/backbone.js"}
     ).then(function(){
         document.querySelector("html").style.opacity = 1;
         // start app?
+        var id = function(element){
+
+            return document.getElementById(element);
+
+        };
+
+        var hero   = id('guy');
+        var ground = id('ground');
+        var groundPosition = {
+            x:0,
+            y:0
+        }//[0 , 0];
+        var heroPosition   = {
+            x:20,
+            y:ground.offsetHeight
+        }//[20 , ground.offsetHeight];   // I want guy to start above the ground
+
+        var plotObject = function(obj , coordinates) {
+            // we relocated our origin point from top left to bottom left to be congruent with a classical schema
+            // we also relocated our origin point of placed objects to the bottom left.
+            obj.style.left = coordinates.x  + 'px';
+            obj.style.top  = document.querySelector('body').offsetHeight - coordinates.y - obj.offsetHeight + 'px';
+        };
+        plotObject(ground , groundPosition ); 
+        plotObject(hero   , heroPosition );
+
+        document.onkeydown=function(e){
+            var evtobj = window.event? event : e
+            
+            if(evtobj.keyCode==37){
+                heroPosition.x += -1;
+            }
+            
+            if(evtobj.keyCode==39){
+                heroPosition.x += 1;
+            }
+        plotObject(hero   , heroPosition );
+            
+           
+        }
     })
 
 }
